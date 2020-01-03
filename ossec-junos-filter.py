cb5261e0e34db11da1a6856583a125bdff9072da
zerealfox <pascal@foxit.pro>
2020-01-03 12:12:51 +0100

    Identity & license text added. Versionning as 0.1

--- ossec-junos-filter.py
+++ ossec-junos-filter.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+#
+#    ossec-junos-filter script for active response for JUNOS devices in ossec
+#    Copyright (C) 2019  Pascal FUKS (pascal at starhackademy.net)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#    Pascal Fuks,
+#    Rue du Cerf, 197
+#    1332 Genval
+#    Belgium
+#    mailto: Pascal at starhackademy.net
+
 from netmiko import Netmiko
 from getpass import getpass
 import textwrap as _textwrap
@@ -67,6 +91,12 @@ def main():
     global bTrace
     bTrace = False
     parser = argparse.ArgumentParser(description="\
+ossec-junos-filter version 0.1, Copyright (C) 2019 Pascal Fuks|n \
+ossec-junos-filter comes with ABSOLUTELY NO WARRANTY|n \
+ (see attached LICENSE file for details).|n \
+This is free software, and you are welcome to redistribute it|n \
+under certain conditions (see attached LICENSE file for details).|n \
+|n \
 Interact with JunOS wazuh.blacklist applied to Ingress interface|n \
 There must be 2 existing policy-options prefix-lists named:|n \
     wazuh.blacklist|n \
